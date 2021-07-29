f3369b051738f6f66e91532cabe7d6a0868b5a11
Jean Morales <jcm_94@live.com>
2021-07-29 02:45:26 -0500

    Created solution and added dependency injection configuration

---
+++ Client/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = ConfigureServices();
+            var serviceProvider = services.BuildServiceProvider();
+
+            var wikimediaProcessor = serviceProvider.GetService<WikimediaProcessor>();
+            wikimediaProcessor.AddActivity<WikimediaExtractor>();
+            wikimediaProcessor.AddActivity<WikimediaTransformer>();
+            wikimediaProcessor.AddActivity<WikimediaLoader>();
+            wikimediaProcessor.Process();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddScoped<IActivity, WikimediaExtractor>();
+            services.AddScoped<IActivity, WikimediaTransformer>();
+            services.AddScoped<IActivity, WikimediaLoader>();
+            services.AddTransient<WikimediaProcessor>();
+            return services;
+        }
+    }
+
+    public class WikimediaLoader : IActivity
+    {
+        public string Message => "";
+
+        public bool Execute()
+        {
+            Console.WriteLine("Loader");
+            return true;
+        }
+    }
+
+    public class WikimediaTransformer : IActivity
+    {
+        public string Message => "";
+
+        public bool Execute()
+        {
+            Console.WriteLine("Transformer");
+            return false;
+        }
+    }
+
+    public class WikimediaExtractor : IActivity
+    {
+        public string Message => "";
+
+        public bool Execute()
+        {
+            Console.WriteLine("Extractor");
+            return true;
+        }
+    }
+
+    public class WikimediaProcessor
+    {
+        private readonly List<Type> _addedActivities = new List<Type>();
+        private readonly IEnumerable<IActivity> _activities;
+
+        public WikimediaProcessor(IEnumerable<IActivity> activities)
+        {
+            _activities = activities;
+        }
+        public void AddActivity<T>() where T : IActivity => _addedActivities.Add(typeof(T));
+        public void Process()
+        {
+            bool continueExecution = true;
+
+            foreach (var activity in _addedActivities)
+            {
+                if (!continueExecution)
+                    return;
+
+                var implementation = _activities.FirstOrDefault(x => x.GetType() == activity);
+                if (implementation == null)
+                    return;
+
+                continueExecution = implementation.Execute();
+            }
+        }
+    }
+
+    public interface IActivity
+    {
+        bool Execute();
+        string Message { get; }
+    }
+}
